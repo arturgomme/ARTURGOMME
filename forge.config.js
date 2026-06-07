f4a9e53ca7bfabb3c8dbb4435bc8d0473cdd8426
arturgomme <arturgomme@icloud.com>
2026-06-07 20:43:25 +0200

    Create forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  makers: [  packagerConfig: {},
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "arturgomme_desktop"
+      }
+    }
+  ]
+};
